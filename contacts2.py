a8edea28e43bab03e8933b58ecf900be239a66e1
Max Brooks <45081361+MaxBrooks114@users.noreply.github.com>
2020-05-07 16:26:41 -0400

    placeholders

--- contacts2.py
+++ contacts2.py
@@ -1,10 +1,11 @@
 import sqlite3
 
 db = sqlite3.connect("contacts.sqlite")
-
-update_sql = "Update contacts set email = 'update@update.com'"
+new_email = 'emails@update.com'
+phone = input("Enter another phone number")
+update_sql = "Update contacts set email = ? where phone = ?"
 update_cursor = db.cursor()
-update_cursor.execute(update_sql)
+update_cursor.execute(update_sql, (new_email, phone))
 print("{} rows were updated".format(update_cursor.rowcount))
 update_cursor.connection.commit()
 update_cursor.close()
