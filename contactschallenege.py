255ff6e8fc2d929cd8856cf04f27fc48f568b823
Max Brooks <45081361+MaxBrooks114@users.noreply.github.com>
2020-05-07 17:05:01 -0400

    string interpolation for tuples

--- contactschallenege.py
+++ contactschallenege.py
@@ -1,10 +1,15 @@
 import sqlite3
 
 db = sqlite3.connect("contacts.sqlite")
-for name, phone, email in db.execute("SELECT * FROM contacts"):
-    print(name)
-    print(phone)
-    print(email)
-    print("=" * 20)
+get_name = input("Who do you want to see?")
+statement = "SELECT * FROM contacts where name LIKE ?"
+for row in db.execute(statement, (get_name,)):
+    print(row)
+#
+# for name, phone, email in db.execute("SELECT * FROM contacts"):
+#     print(name)
+#     print(phone)
+#     print(email)
+#     print("=" * 20)
 
 db.close()
