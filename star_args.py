50d9d5910dce697a33efb76ded217987f5ed75a8
Max Brooks <45081361+MaxBrooks114@users.noreply.github.com>
2020-05-13 11:59:58 -0400

    subclass scrollbox using inheritance

--- star_args.py
+++ star_args.py
@@ -1,18 +1,5 @@
-def average(*args):
-    print(type(args))
-    print("args is {}".format(args))
-    print("*args is:", *args)
-    mean = 0
+def print_backwards(*args, **kwargs):
+    for word in args[::-1]:
+        print(word[::-1], **kwargs)
 
-    for arg in args:
-        mean += arg
-    return mean / len(args)
-
-
-print(average(1,2,3,4))
-
-
-def tupleize(*args):
-   return args
-
-print(tupleize(1,2,4,5))
+print_backwards("hello", "dude", "how", "are", "you", end='\n')
